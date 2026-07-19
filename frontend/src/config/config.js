5bc6f54372299af37488ba3d3b126810166cc210
Chakravarti-pranjal <chakravartipranjal3@gmail.com>
2026-07-19 23:45:23 +0530

    base url updated

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,6 +1,7 @@
 import toast from "react-hot-toast";
 
-export const baseUrl = "http://localhost:4000/api/v1";
+// export const baseUrl = "http://localhost:4000/api/v1";
+export const baseUrl = "https://lovingo.duckdns.org/api/v1";
 
 export const notifySuccess = (msg) => toast.success(msg);
 export const notifyError = (msg) => toast.error(msg);
